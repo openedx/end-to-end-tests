18d6cb4793a272ac652b8b8776501e115210ac61
Tycho Hob <ty@axim.org>
2026-09-15 15:11:52 -0400

    feat: Add instructor-dashboard, certificates, analytics capabilities

--- src/config/selectors/index.ts
+++ src/config/selectors/index.ts
@@ -43,4 +43,14 @@ export { STUDIO_PAGES_RESOURCES_SELECTORS } from './studio-pages-resources';
 export { STUDIO_CUSTOM_PAGES_SELECTORS } from './studio-custom-pages';
 export { STUDIO_UNIT_PAGE_SELECTORS } from './studio-unit';
 export { STUDIO_EDITOR_SELECTORS } from './studio-editors';
-export { COURSE_CREATOR_ADMIN_SELECTORS } from './django-admin';
+export {
+  CERTIFICATE_GENERATION_ADMIN_SELECTORS,
+  COURSE_CREATOR_ADMIN_SELECTORS,
+} from './django-admin';
+export {
+  INSTRUCTOR_DASHBOARD_SELECTORS,
+  INSTRUCTOR_REPORT_ROWS,
+  INSTRUCTOR_TAB_IDS,
+  instructorTabLink,
+  instructorTabPath,
+} from './instructor';
